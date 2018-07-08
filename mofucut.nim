81e0b494fdf46f4efdfbf96cf2226e44af29d471
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-07-09 00:35:56 +0900

    Update mofucut.nim

--- mofucut.nim
+++ mofucut.nim
@@ -1,13 +1,16 @@
 import system, os, strutils, unicode
 
-if isMainModule and paramCount() == 2:
-  let begin = os.commandLineParams()[0]
-  let finish = os.commandLineParams()[1]
-  echo runeSubStr(
-    readLine stdin,
-    if begin == "199000万": 1990000000 else: parseInt begin,  # Why is this supporting "199000万"? Check out the links below!
-    if finish == "199000万": 1990000000 else: parseInt finish # <https://misskey.xyz/notes/5b3dc2e2625ee930f83a1dbc> by @2vg
-  )
-else:
+try:
+  if isMainModule and paramCount() == 2:
+    let begin = os.commandLineParams()[0]
+    let finish = os.commandLineParams()[1]
+    echo runeSubStr(
+      readLine stdin,
+      if begin == "199000万": 1990000000 else: parseInt begin,  # Why is this supporting "199000万"? Check out the links below!
+      if finish == "199000万": 1990000000 else: parseInt finish # <https://misskey.xyz/notes/5b3dc2e2625ee930f83a1dbc> by @2vg
+    )
+  else:
+    raise newException(ValueError, "")
+except:
   stderr.writeLine "Usage: mofucut STARTPOS ENDPOS"
   system.programResult = 1
